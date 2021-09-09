1fb4ab8002aec3573fc4cbca6aab472317417798
Marc-Antoine Argenton <maargenton.dev@gmail.com>
2021-09-08 23:59:47 -0700

    - Fix test failing on windows -- one more path to convert to native

--- pkg/cli/completion_test.go
+++ pkg/cli/completion_test.go
@@ -66,7 +66,7 @@ func TestDefaultCompletion(t *testing.T) {
 			})
 		})
 		t.Run("when Calling DefaultCompletion() with partial unique folder name", func(t *testing.T) {
-			suggestions := cli.DefaultCompletion("../cl")
+			suggestions := cli.DefaultCompletion(filepath.FromSlash("../cl"))
 			t.Run("then suggestions include the files in that folder", func(t *testing.T) {
 				require.That(t, suggestions).Field("Option").IsSupersetOf([]string{
 					filepath.FromSlash("../cli/completion.go"),
